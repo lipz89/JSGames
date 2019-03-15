3cc1ad8d441cde0d42e6282576f131ad9900d5a6
23955618 <23955618@qq.com>
2019-03-15 15:07:09 +0800

    整理提交

---
+++ common.js
@@ -0,0 +1,24 @@
+﻿
+var store = {
+    set: function (key, item) {
+        var str = JSON.stringify(item);
+        window.localStorage.setItem(key, str);
+    },
+    get: function (key) {
+        var str = window.localStorage.getItem(key);
+        return JSON.parse(str);
+    },
+    remove: function (key) {
+        window.localStorage.removeItem(key);
+    }
+};
+var $ = function (id) {
+    return document.getElementById(id);
+}
+
+function ReturnFalse() {
+    return false;
+}
+
+document.onselectstart = ReturnFalse;
+document.oncontextmenu = ReturnFalse;
---
+++ menu.js
@@ -0,0 +1,47 @@
+﻿var pages={
+    menuPage: {
+            file: "index.html"
+    },
+    gamePages: [
+        {
+            file:"49.html",
+            name:"马踏棋盘"
+        },
+        {
+            file:"49c.html",
+            name:"马踏棋盘绘图版"
+        },
+        {
+            file:"bbh.html",
+            name:"八宝盒"
+        },
+        {
+            file:"JSSudoku.html",
+            name:"数独"
+        },
+        {
+            file:"light.html",
+            name:"九宝莲灯"
+        },
+        {
+            file:"winmine.html",
+            name:"扫雷"
+        },
+        {
+            file: "eight.html",
+            name: "八数码"
+        },
+        {
+            file: "eightAStar.html",
+            name: "八数码A*算法演示"
+        },
+        {
+            file: "JSSnake.html",
+            name: "贪吃蛇"
+        },
+        {
+            file: "JSPushBox.html",
+            name: "推箱子"
+        }
+    ]
+}
